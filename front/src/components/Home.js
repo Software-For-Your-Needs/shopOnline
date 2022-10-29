b3f2e71522abb90c53368bbde856566665dc7336
lstiven18 <mr.leon120@hotmail.com>
2022-10-29 15:40:26 -0500

    feat: accede a los productos desde base de datos

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -2,135 +2,59 @@ import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/Metadata'
 import { useDispatch } from 'react-redux'
 import { getProducts } from '../actions/productActions'
+import { useSelector } from 'react-redux'
+import {Link} from 'react-router-dom'
+import { useAlert } from 'react-alert'
 
 export const Home = () => {
+    const {loading, productos, error} = useSelector(state=>state.products)
+    const alert = useAlert();
     const dispatch = useDispatch();
+
     useEffect(()=>{
+        if (error){
+            return alert.error(error)
+        }
         dispatch(getProducts());
+        alert.success("OK")
     }, [dispatch])
     return (
         <Fragment>
-            <MetaData title="Productos de alta calidad"></MetaData>
-            <h1 id="encabezado_productos">Productos</h1>
-
-            <section id="productos" className='container mt-5'>
-                <div className='row'>
-                    {/*Producto 1*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Caneca.jpg' alt="Caneca"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Caneca 5 Gal</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
+            {loading ? <h1>Cargando...</h1>: (
+                <Fragment>
+                    <MetaData title="Productos de alta calidad"></MetaData>
+                    <h1 id="encabezado_productos">Productos</h1>
+        
+                    <section id="productos" className='container mt-5'>
+                        <div className='row'>
+                            {productos && productos.map(producto => (
+                                <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                                    <div className='card p-3 rounded'>
+                                        <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
+                                        <div className='card-body d-flex flex-column'>
+                                            <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
+                                            <div className='rating mt-auto'>
+                                                <div className='rating-outer'>
+                                                    <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
+                                                </div>
+                                                <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
+                                            </div>
+                                            <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
+                                                Ver detalle
+                                            </Link>
+                                        </div>
                                     </div>
-                                    <span id="No_de_opiniones"> 5 reviews</span>
                                 </div>
-                                <p className='card-text'>$75.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
+                            ))}
+                            
+        
                         </div>
-                    </div>
-
-                    {/*Producto 2*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Contenedor.png' alt="Contenedor"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Contenedor</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 2 reviews</span>
-                                </div>
-                                <p className='card-text'>$900.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 3*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Cunete.jpg' alt="Cuñete"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cuñete</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 12 reviews</span>
-                                </div>
-                                <p className='card-text'>$37.500</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 4*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Galon.jpg' alt="Galon"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Galón</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 7 reviews</span>
-                                </div>
-                                <p className='card-text'>$4.750</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 5*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Garrafa.png' alt="Garrafa"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Garrafa</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 7 reviews</span>
-                                </div>
-                                <p className='card-text'>$37.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    {/*Producto 6*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Tambor.jpg' alt="Tambor"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Tambor</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 7 reviews</span>
-                                </div>
-                                <p className='card-text'>$145.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </section>
+                    </section>
+                
+                </Fragment>
+                
+            )}
+            
 
         </Fragment>
     )
