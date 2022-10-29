ecc4e4e178e0b16b796a1fd51380f5316df76821
lstiven18 <mr.leon120@hotmail.com>
2022-10-29 11:09:36 -0500

    feat: contacto con el back

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,8 +1,16 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import MetaData from './layout/Metadata'
+import { useDispatch } from 'react-redux'
+import { getProducts } from '../actions/productActions'
 
 export const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getProducts());
+    }, [dispatch])
     return (
         <Fragment>
+            <MetaData title="Productos de alta calidad"></MetaData>
             <h1 id="encabezado_productos">Productos</h1>
 
             <section id="productos" className='container mt-5'>
