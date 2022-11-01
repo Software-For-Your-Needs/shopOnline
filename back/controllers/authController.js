7eaa78d75a448107b162eb0c40282a1504d881a0
lstiven18 <mr.leon120@hotmail.com>
2022-10-31 21:35:44 -0500

    refactor: modifica avatar

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -13,7 +13,7 @@ exports.registroUsuario= catchAsyncErrors(async (req, res, next) =>{
         password,
         avatar:{
             public_id:"ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp",
-            url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp=CAU"
+            url:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9e1ae83e-cf8b-44d0-b902-e0e53e219584/d5dy73n-857ab4b2-8cfa-4c08-92a6-8989e286ee2f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzllMWFlODNlLWNmOGItNDRkMC1iOTAyLWUwZTUzZTIxOTU4NFwvZDVkeTczbi04NTdhYjRiMi04Y2ZhLTRjMDgtOTJhNi04OTg5ZTI4NmVlMmYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.EsnJ3dKZhs6yJwi8js_NlsbekIYdOf87e44Y9sU4LnA"
         }
     })
 
