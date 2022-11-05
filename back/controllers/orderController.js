53d03629d4f6984ccc449fa918753fb7945e6b25
lstiven18 <mr.leon120@hotmail.com>
2022-11-05 09:39:48 -0500

    feat: métodos para ver una y todas las ordenes

--- back/controllers/orderController.js
+++ back/controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order=require("../models/order")
 const Product=require("../models/productos")
-const catchAsyncErrors=require("../middleware/catchAsyncErrors")
+const catchAsyncErrors=require("../middleware/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorHandler");
 
 // Crear una orden
 exports.newOrder= catchAsyncErrors(async(req, res, next)=>{
@@ -29,5 +30,28 @@ exports.newOrder= catchAsyncErrors(async(req, res, next)=>{
     res.status(201).json({
         success: true,
         order
+    })
+})
+
+//Ver una orden
+exports.getOneOrder= catchAsyncErrors(async(req, res, next)=>{
+    const order= await Order.findById(req.params.id).populate("user", "nombre email") //restriccion de usuario
+
+    if(!order){
+        return next(new ErrorHandler("No encontramos una orden con ese Id",404))
+    }
+
+    res.status(200).json({
+        success:true,
+        order
+    })
+})
+// Ver todas mis ordenes (usuario logueado)
+exports.myOrders=catchAsyncErrors(async(req, res, next)=>{
+    const orders = await Order.find({user:req.user.id});
+
+    res.status(200).json({
+        success:true,
+        orders
     })
 })
--- back/routes/orders.js
+++ back/routes/orders.js
@@ -1,10 +1,12 @@
 const express=require("express");
-const { newOrder } = require("../controllers/orderController");
+const { newOrder, getOneOrder, myOrders } = require("../controllers/orderController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router=express.Router();
 
 
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder)
+router.route("/order/:id").get(isAuthenticatedUser, getOneOrder)
+router.route("/orders/me").get(isAuthenticatedUser, myOrders)
 
 module.exports = router;
