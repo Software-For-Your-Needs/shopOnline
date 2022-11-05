fea998b98a4f608fa4758a40b8a26d8ecaf01e39
lstiven18 <mr.leon120@hotmail.com>
2022-11-04 19:53:46 -0500

    feat: integra correo hotmail para recuperar contraseña

--- back/utils/sendEmail.js
+++ back/utils/sendEmail.js
@@ -2,15 +2,15 @@ const nodemailer=require("nodemailer");
 
 const sendEmail = async options =>{
     const transport = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+        host: "smtp.office365.com",
+        port: 587,
         auth: {
-          user: "dd9d3b70858cbe",
-          pass: "4a1db491a9e69d"
+          user: "carlostello_25@hotmail.com",
+          pass: "wswmgcewuuqykkdf"
         }
       });
       const mensaje = {
-        from: "ShopOnline Store <noreply@shoponline.com>",
+        from: "ShopOnline Store <carlostello_25@hotmail.com>",
         to: options.email,
         subject: options.subject,
         text: options.mensaje
